59111d1bd08c8448510415455f0860394c33b0bf
tc-imba <liuyh615@sjtu.edu.cn>
2017-09-03 16:51:25 +0800

    p1 complete

---
+++ project/p1/problem/Makefile-Demo/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+    print_hello();
+    cout << endl;
+    cout << "The factorial of 5 is " << factorial(5) << endl;
+    return 0;
+}
